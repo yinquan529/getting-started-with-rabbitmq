c265ec4ebf9de5e64a19b32be066ad3ff4718e75
longhairedsi@gmail.com <longhairedsi@gmail.com@86d59608-b1c3-4699-1dc6-103582668d73>
2011-05-08 15:42:55 +0000

    tutorial 1 - hello world

---
+++ trunk/GettingStartedWithRabbitMQ/Example1HelloWorld/Consumer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+
+namespace GettingStartedWithRabbitMQ.Example1HelloWorld
+{
+    public class Consumer
+    {
+        protected IModel Model;
+        protected IConnection Connection;
+        protected string QueueName;
+
+        protected bool isConsuming;
+
+        // used to pass messages back to UI for processing
+        public delegate void onReceiveMessage(byte[] message);
+        public event onReceiveMessage onMessageReceived;
+
+        public Consumer(string hostName, string queueName)
+        {
+            QueueName = queueName;
+            ConnectionFactory connectionFactory = new ConnectionFactory();
+            connectionFactory.HostName = hostName;
+            Connection = connectionFactory.CreateConnection();
+            Model = Connection.CreateModel();
+            Model.QueueDeclare(QueueName, false, false, false, null);
+        }
+
+        //internal delegate to run the consuming queue on a seperate thread
+        private delegate void ConsumeDelegate();
+
+        public void StartConsuming()
+        {
+            isConsuming = true;
+            ConsumeDelegate c = new ConsumeDelegate(Consume);
+            c.BeginInvoke(null, null);
+        }
+
+        public void Consume()
+        {
+            QueueingBasicConsumer consumer = new QueueingBasicConsumer(Model);
+            String consumerTag = Model.BasicConsume(QueueName, false, consumer);
+            while (isConsuming)
+            {
+                try
+                {
+                    RabbitMQ.Client.Events.BasicDeliverEventArgs e = (RabbitMQ.Client.Events.BasicDeliverEventArgs)consumer.Queue.Dequeue();
+                    IBasicProperties props = e.BasicProperties;
+                    byte[] body = e.Body;
+                    // ... process the message
+                    onMessageReceived(body);
+                    Model.BasicAck(e.DeliveryTag, false);
+                }
+                catch (OperationInterruptedException ex)
+                {
+                    // The consumer was removed, either through
+                    // channel or connection closure, or through the
+                    // action of IModel.BasicCancel().
+                    break;
+                }
+            }
+
+        }
+
+        public void Dispose()
+        {
+            isConsuming = false;
+            if (Connection != null)
+                Connection.Close();
+            if (Model != null)
+                Model.Abort();
+        }
+    }
+}
---
+++ trunk/GettingStartedWithRabbitMQ/Example1HelloWorld/Producer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace GettingStartedWithRabbitMQ.Example1HelloWorld
+{
+    public class Producer : IDisposable
+    {
+
+        protected IModel Model;
+        protected IConnection Connection;
+        protected string QueueName;
+
+        public Producer(string hostName, string queueName)
+        {
+            QueueName = queueName;
+            var connectionFactory = new ConnectionFactory();
+            connectionFactory.HostName = hostName;
+            Connection = connectionFactory.CreateConnection();
+            Model = Connection.CreateModel();
+            Model.QueueDeclare(QueueName, false, false, false, null);
+        }
+
+        public void SendMessage(byte[] message)
+        {
+            IBasicProperties basicProperties = Model.CreateBasicProperties();
+            Model.BasicPublish("", QueueName, basicProperties, message);
+        }
+
+        public void Dispose()
+        {
+            if (Connection != null)
+                Connection.Close();
+            if (Model != null)
+                Model.Abort();
+        }
+    }
+}
