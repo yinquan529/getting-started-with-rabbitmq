d08a7eed7b688a22f9733a0b4edba821819c74bc
longhairedsi@gmail.com <longhairedsi@gmail.com@86d59608-b1c3-4699-1dc6-103582668d73>
2011-05-10 23:17:41 +0000

    example 2  - work queues added

--- trunk/GettingStartedWithRabbitMQ/Program.cs
+++ trunk/GettingStartedWithRabbitMQ/Program.cs
@@ -14,7 +14,7 @@ namespace GettingStartedWithRabbitMQ
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new _2_Work_Queues());
         }
     }
 }
