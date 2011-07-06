1e7b650901e151b818f6c21d328522414f58740b
longhairedsi@googlemail.com <longhairedsi@googlemail.com@86d59608-b1c3-4699-1dc6-103582668d73>
2011-07-06 13:08:19 +0000

    Added initial Tutorial 4 files

---
+++ GettingStartedWithRabbitMQ/Example4Routing/4-Routing-Producer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GettingStartedWithRabbitMQ.Example4Routing
+{
+    public partial class _4_Routing_Producer : Form
+    {
+        public _4_Routing_Producer()
+        {
+            InitializeComponent();
+        }
+    }
+}
